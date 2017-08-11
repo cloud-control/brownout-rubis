dec97f67d84c58f4701639f8ef9f27a38f74d3ce
Cristian Klein <cristiklein@gmail.com>
2017-08-11 14:25:53 +0200

    Added controller interface

---
+++ src/controller.h
@@ -0,0 +1,13 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+struct controller_s;
+typedef struct controller_s *controller_t;
+
+controller_t controller_init();
+void controller_free(controller_t);
+void controller_report_arrival(controller_t c);
+void controller_report_departure(controller_t c, double response_time, int with_optional);
+int controller_with_optional(controller_t c, int current_queue_length);
+
+#endif // CONTROLLER_H
