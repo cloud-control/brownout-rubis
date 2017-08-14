e3ca00c2f6b4dcf56b27a203dc9ab5f2cc0775ce
Cristian Klein <cristiklein@gmail.com>
2017-08-14 12:18:17 +0200

    Load controller (WIP)

--- src/controller.c
+++ src/controller.c
@@ -19,26 +19,63 @@ static double now()
 #define LOG_ERROR(fmt, ...) LOG("error", fmt, ##__VA_ARGS__)
 
 struct controller_s {
-    PyObject *pModule;
+    PyObject *pController;
 };
+
+#define ABORT_WITH_PYERR(fmt, ...) \
+    do { \
+        if (PyErr_Occurred()) \
+            PyErr_Print(); \
+        LOG_ERROR(fmt, ##__VA_ARGS__); \
+        exit(1); \
+    } while(0)
 
 controller_t controller_init() {
     controller_t c = malloc(sizeof(struct controller_s));
+    char fake_argv[] = { "" };
 
+    Py_SetProgramName(fake_argv[0]);
     Py_Initialize();
+    PySys_SetArgv(0, fake_argv);
+
     PyObject* sysPath = PySys_GetObject((char*)"path");
     PyObject* curDir = PyString_FromString("./brownout-lb-simulator");
     PyList_Insert(sysPath, 0, curDir);
     Py_DECREF(curDir);
 
     PyObject *pName = PyString_FromString("controllers.server.cf_cascaded");
-    c->pModule = PyImport_Import(pName);
+    PyObject *pModule = PyImport_Import(pName);
     Py_DECREF(pName);
 
-    if (c->pModule == 0) {
-        LOG_ERROR("Could not load Python module; aborting");
-        exit(1);
-    }
+    if (pModule == 0)
+        ABORT_WITH_PYERR("Could not load Python module; aborting");
+
+    PyRun_SimpleString(
+        "import argparse\n"
+        "import controllers.server.cf_cascaded as controllerModule\n"
+        "parser = argparse.ArgumentParser()\n"
+        "controllerModule.addCommandLine(parser)\n"
+        "args = parser.parse_args()\n"
+        "controllerModule.parseCommandLine(args)\n"
+    );
+
+    PyObject *pNewInstance = PyObject_GetAttrString(pModule, "newInstance");
+    if (pNewInstance == 0)
+        ABORT_WITH_PYERR("Could not find newInstance in Python controller; aborting");
+    Py_DECREF(pModule);
+
+    if (PyCallable_Check(pNewInstance) == 0)
+        ABORT_WITH_PYERR("newInstance in Python controller is not callable; aborting");
+
+    PyObject *pArgs = PyTuple_New(2);
+    PyTuple_SetItem(pArgs, 0, Py_None);
+    PyTuple_SetItem(pArgs, 1, Py_None);
+    c->pController = PyObject_CallObject(pNewInstance, pArgs);
+    Py_DECREF(pArgs);
+    Py_DECREF(pNewInstance);
+
+    if (c->pController == 0)
+        ABORT_WITH_PYERR("Could not create new instance of controller; aborting");
 
     return c;
 }
@@ -64,6 +101,5 @@ char *controller_upstream_info(controller_t c) {
 }
 
 void controller_run_control_loop(controller_t c) {
-    if (!c->pModule) return;
     fprintf(stderr, "%s\n", __FUNCTION__);
 }
