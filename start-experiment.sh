928c49469f66899747dbcf21cbce6af33d6a4bbf
Cristian Klein <cristiklein@gmail.com>
2017-08-24 11:43:06 +0200

    Tommi's scenario

--- start-experiment.sh
+++ start-experiment.sh
@@ -34,14 +34,14 @@ log "Starting rubis-db-tier ..."
 docker run --detach \
     --name rubis-db-tier-0 \
     --volume $DB_DIR:/var/lib/mysql \
-    --cpuset-cpus '0-1' \
+    --cpuset-cpus '0' \
     rubis-db-tier
 log "Starting rubis-web-tier ..."
 docker run --detach \
     --name rubis-web-tier-0 \
     --link rubis-db-tier-0:mysql \
     --publish 8080:80 \
-    --cpuset-cpus '0-1' \
+    --cpuset-cpus '0' \
     rubis-web-tier
 
 # Wait for db-tier to start
@@ -63,13 +63,71 @@ docker run --detach \
     rubis-control-tier
 
 log "Starting httpmon ..."
-docker run --rm -ti \
+
+#
+# Experiment verbs (see below for usage)
+#
+function setStart {
+	echo [`date +%s`] start
+}
+function setCount {
+	echo [`date +%s`] count=$1
+	echo "count=$1" >&9
+}
+function setOpen {
+	echo [`date +%s`] open=$1
+	echo "open=$1" >&9
+}
+function setThinkTime {
+	echo [`date +%s`] thinktime=$1
+	echo "thinktime=$1" >&9
+}
+function setConcurrency {
+	echo [`date +%s`] concurrency=$1
+	echo "concurrency=$1" >&9
+}
+function setTimeout {
+	echo [`date +%s`] timeout=$1
+	echo "timeout=$1" >&9
+}
+
+#
+# Initialization
+#
+
+# Create FIFO to communicate with httpmon and start httpmon
+rm -f httpmon.fifo
+mkfifo httpmon.fifo
+docker run --rm -i \
     --name rubis-wg-0 \
     --link rubis-control-tier-0:host \
     cklein/httpmon \
     --url http://host/PHP/RandomItem.php \
-    --open \
     --deterministic \
-    --concurrency 100 \
-    --thinktime 1 \
-| sed -r -e 's/^(time=[0-9.]+).*(latency95=[0-9.]+ms).*(rr=[0-9.]+%).*$/\1\t\2\t\3/p'
+    --concurrency 0 \
+    < httpmon.fifo &> httpmon.log &
+exec 9> httpmon.fifo
+
+#
+# Initialize experiment
+#
+setOpen 1
+setThinkTime 1
+setTimeout 4
+setStart
+
+for i in $(seq 1 20); do
+    setConcurrency  20
+    sleep 60
+    setConcurrency  100
+    sleep 60
+    setConcurrency  30
+    sleep 60
+    setConcurrency  70
+    sleep 60
+    setConcurrency  20
+    sleep 60
+done
+setConcurrency  0
+
+docker logs rubis-control-tier-0 > rubis-control-tier-0.log
